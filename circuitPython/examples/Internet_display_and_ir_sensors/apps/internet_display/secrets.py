07ed62a3b06f6503bfb7902e621f94d0fc03cd21
Tuan Nguyen <tuan.nguyen@brtchip.com>
2021-06-28 17:13:31 +0700

    Zipping secret.py

--- circuitPython/examples/Internet_display_and_ir_sensors/apps/internet_display/secrets.py
+++
@@ -1,9 +0,0 @@
-wifi_secrets=[
-    {'ssid':'Tuan', 'password':'0973907059'},
-    {'ssid':'nah', 'password':'abc12345'},
-]
-
-aio_secrets={
-    'aio_username':'___',
-    'aio_key':'___' # key will get disable automatically if push into by github
-}
