47a8fd738f818ebbdabac6802bbec8bfbb6d5b42
Tuan Nguyen <tuan.nguyen@brtchip.com>
2021-10-04 17:11:26 +0700

    Add volume when play video
    Related #6

--- circuitPython/lib/brteve/brt_eve_movie_player.py
+++ circuitPython/lib/brteve/brt_eve_movie_player.py
@@ -23,7 +23,7 @@ class BrtEveMoviePlayer:
             print("Cannot switch flash to full mode")
             return self
 
-        self.flag = eve.OPT_FLASH | eve.OPT_FULLSCREEN | eve.OPT_NOTEAR
+        self.flag = eve.OPT_FLASH | eve.OPT_FULLSCREEN | eve.OPT_NOTEAR | eve.OPT_SOUND
         eve.cmd_flashsource(flash_address)
         return self
 
@@ -39,7 +39,7 @@ class BrtEveMoviePlayer:
         self.write_pointer = 0
         eve.cmd_regwrite(eve.REG_MEDIAFIFO_WRITE, 0)
 
-        self.flag = eve.OPT_MEDIAFIFO | eve.OPT_FULLSCREEN | eve.OPT_NOTEAR
+        self.flag = eve.OPT_MEDIAFIFO | eve.OPT_FULLSCREEN | eve.OPT_NOTEAR | eve.OPT_SOUND
         return self
 
     def set_flag(self, flag):
@@ -50,9 +50,18 @@ class BrtEveMoviePlayer:
         """ Append cmd_playvideo flag"""
         self.flag |= flag
 
+    def set_volume(self, vol):
+        """ Set system volume"""
+        eve = self.eve
+        eve.wr8(eve.REG_VOL_PB, vol)
+        
     def play(self):
         """ Start playback a movie"""
         eve = self.eve
+        eve.wr8(eve.REG_GPIOX_DIR,0xFF)
+        eve.wr8(eve.REG_GPIOX,0xFF)
+
+        self.set_volume(0xFF)
         eve.cmd_playvideo(self.flag)
 
         if self.source == _SOURCE_FILE:
@@ -61,6 +70,8 @@ class BrtEveMoviePlayer:
             with open(self.file, "rb") as file_handler:
                 while not eve.is_finished():
                     self._file_service(file_handler)
+
+        eve.Nop()
         eve.finish()
 
     def _file_service(self, file_handler):
